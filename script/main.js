43a2602da37945b50693b49f09504d28f11416db
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-11-18 18:53:35 +0100

    still working

--- script/main.js
+++ script/main.js
@@ -2,6 +2,13 @@ const metricRadio = document.getElementById("metric");
 const imperialRadio = document.getElementById("imperial");
 const metricUnit = document.getElementById("metric_data");
 const imperialUnit = document.getElementById("imperial_data");
+const imperialHeightFt = document.getElementById("imperial_height_ft");
+const imperialHeightIn = document.getElementById("imperial_height_in");
+const weight_input = document.getElementById("weight");
+const form_metric = document.getElementById("metric_data");
+const form_imperial = document.getElementById("imperial_data");
+const heightCm = document.getElementById("height_cm")
+const weightKg = document.getElementById("weight_kg")
 
 imperialRadio.addEventListener("change", isImperialChecked)
 metricRadio.addEventListener("change", isImperialChecked)
