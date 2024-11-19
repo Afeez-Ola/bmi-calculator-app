0bc44d1ae846482124042113dd5ad1a224026b01
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-11-19 16:33:45 +0100

    calculated the bmi

--- script/main.js
+++ script/main.js
@@ -7,6 +7,9 @@ const imperialUnit = document.getElementById("imperial_data");
 const metricDiv = document.getElementById("metric_data");
 const ImperialDiv = document.getElementById("imperial_data");
 
+// weight Note
+const weightNote = document.getElementById("weight_note");
+
 
 // The BMI Result
 const bmiResult = document.getElementById("result");
@@ -29,10 +32,10 @@ const weightLbs = document.getElementById("weight_lbs");
 
 imperialRadio.addEventListener("change", isImperialChecked);
 metricRadio.addEventListener("change", isImperialChecked);
-height_form_metric.addEventListener("keyup", getHeightFormMetric)
-weight_form_metric.addEventListener("keyup", getWeightFormMetric)
+height_form_metric.addEventListener("input", metricBmiCalculation)
+weight_form_metric.addEventListener("input", metricBmiCalculation)
 
-metricDiv.addEventListener("change", metricBmiCalculation(getHeightFormMetric(),getWeightFormMetric()))
+// metricDiv.addEventListener("change", metricBmiCalculation(getHeightFormMetric,getWeightFormMetric))
 
 function isImperialChecked(e){
   e.preventDefault();
@@ -47,17 +50,26 @@ function isImperialChecked(e){
   }
 }
 
-function getHeightFormMetric(){
-  let height = int(heightCm.value)/100
-  return height
-}
-function getWeightFormMetric(){
-  let weight = int(weightKg.value)
-  return weight
-}
 
-function metricBmiCalculation(height, weight){
-  console.log(height, weight);
-  bmiResult.innerText = (weight * weight)/height
+
+function metricBmiCalculation(){
+  const height = (parseFloat(heightCm.value)/100);
+  const weight = parseFloat(weightKg.value);
+
+  if (height > 0 && weight > 0){
+    const bmi = (weight / (height * height)).toFixed(1)
+
+    if (bmi < 18.5){
+      weightNote.innerText = "Underweight"
+    } else if (bmi == 18.5 || bmi <= 24.9) {
+      weightNote.innerText = "Healthy"
+    } else if (bmi == 25 || bmi <= 29.9) {
+      weightNote.innerText = "Overweight"
+    } else{
+      weightNote.innerText = "Obese"
+    }
+
+    bmiResult.innerText = bmi
+  }
 }
 
